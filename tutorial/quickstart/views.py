878052d621ac98b9286cb59733a14b57f9dd9cec
Xartiou <you@example.comxartiou@gmail.com>
2022-07-16 08:18:24 +0300

    Quick start DRF

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, позволяющий просматривать или редактировать пользователей
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, позволяющий просматривать или редактировать группы
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
